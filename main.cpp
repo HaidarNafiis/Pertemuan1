3a7619427951caece4407e15d0b501eacda63146
HaidarNafiis <112888428+HaidarNafiis@users.noreply.github.com>
2022-09-12 21:09:29 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,21 +1,13 @@
 #include <iostream>
-#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    /*
-     printf("test");
-    */
-    int y = 10*10/5-2;
-    float x = 1000/2-420+10*10+3;
-    string z = " XXl";
-
-    cout << "umur saya :" << y;
-    cout << endl;
-    cout << "\nTinggi badan :" << x;
-    cout << "\nUkuran baju :" << z;
+    cout << "Nama         : Haidar Nafiis Usmany" << endl;
+    cout << "NIM          : A11.2022.14146" << endl;
+    cout << "Alamat       : Tlogosari" << endl;
+    cout << "Asal Sekolah : SMK 8 SEMARANG" << endl;
 
     return 0;
 }
