6e3d692c7217d138bd1b1d4e2242fb18a8312fd6
HaidarNafiis <112888428+HaidarNafiis@users.noreply.github.com>
2022-09-12 21:27:32 +0700

    Add files via upload

---
+++ main latihan 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    /*
+     printf("test");
+    */
+    int y = 10*10/5-2;
+    float x = 1000/2-420+10*10+3;
+    string z = " XXl";
+
+    cout << "umur saya :" << y;
+    cout << endl;
+    cout << "\nTinggi badan :" << x;
+    cout << "\nUkuran baju :" << z;
+
+    return 0;
+}
